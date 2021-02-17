c3bf8322b8036dd6088f4ab4f55690b7a6ec917f
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-17 22:54:18 +0530

    axios added and login successful

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+ 
+  username:String = "";
+  password:String = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  login():void{
+     console.log(this.username,this.password);
+     axios.post("http://localhost:8015/bdaybumps/authenticate",{username:this.username,password:this.password}).then(
+       (Response)=>{
+         console.log(Response);
+         window.localStorage.setItem("jwt",Response.data.jwt);
+       }
+     ).catch(
+       (error)=>{
+         console.log("unable to login");
+       }
+     )
+
   }
 
 }
