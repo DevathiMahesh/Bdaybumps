0919f8bb9231ae7958624e840c2534522e097cee
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-18 21:24:20 +0530

    icons added to feed

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import {faTrashAlt,faDownload,faCameraRetro} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
-
+  faTrash = faTrashAlt;
+  faDownload = faDownload;
+  faMemory = faCameraRetro;
   constructor() { }
 
   ngOnInit(): void {
