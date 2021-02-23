5f3dd9f1ab6d04c8e1ff59a58f0d9c588cf51496
DevathiMahesh <d.mahesh995@gmail.com>
2021-02-23 22:49:34 +0530

    Navigation added to menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {faPlus,faImage} from '@fortawesome/free-solid-svg-icons'
+import { ActivatedRoute, Router } from '@angular/router';
+import { faPlus, faImage } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
   faplus = faPlus;
   faImage = faImage;
-  litems = ["My Profile","Friends","Bestiees","Memories","Gallery","Gifts","Orders"]
-  constructor() { }
+  litems = [
+    { title: 'My Profile', route: 'profile' },
+    { title: 'Friends', route: 'profile' },
+    {title:'Bestiees',route:'profile'},
+    {title:'Memories',route:'memories'},
+    {title:'Gallery',route:'gallery'},
+    {title:'Gifts',route:'gifts'},
+    {title:'Orders',route:'orders'}
+  ];
+  constructor(private router:Router) {}
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void {}
+  navigate(item):void{
+     this.router.navigate([item.route])
   }
-
 }
